ebcba85771c9c80972dde5c60f4e369893a37ac6
钟履桂 <zhonglvgui@magicengine.com.cn>
2025-03-27 14:02:53 +0800

    fix: alipayPlus reference search

--- apps/checkout-app/src/app/payorder/page.tsx
+++ apps/checkout-app/src/app/payorder/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from "next/navigation";
 type SearchParams = Promise<{ reference: string }>;
 export default async function Home(props: { searchParams: SearchParams }) {
   const searchParams = await props.searchParams
-  redirect(`/alipayPlus/${searchParams.reference}`);
+  redirect(`/alipayPlus/?reference=${searchParams.reference}`);
 }
